bc6021bd1ab7019e08d6715236e99f1b9b7df50a
Jesper Annebäck <jesper.anneback@nordnet.se>
2018-04-25 11:31:17 +0200

    mvp done

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const mainMenu = [
+  { text: 'New',    url: 'https://news.ycombinator.com/newest' },
+  { text: 'Show',   url: 'https://news.ycombinator.com/show' },
+  { text: 'Submit', url: 'https://news.ycombinator.com/submit' },
+];
+
+export const Header = () => (
+  <div>
+    hejejejej
+  </div>
+)
---
+++ src/components/page/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+import { Grid, Row, Col } from '@nordnet/grid';
+
+export const Page = ({ children }) => (
+  <Grid>
+    <Row>
+      <Col xs={12} sm={8} md={6} lg={4}>
+        { children }
+      </Col>
+    </Row>
+  </Grid>
+)
