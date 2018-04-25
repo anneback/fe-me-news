c240d1e7d7a9e060fd8c3ed675788026be2411c3
Jesper Annebäck <jesper.anneback@nordnet.se>
2018-04-25 15:37:05 +0200

    added layout, and removed Grid

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,13 +1,5 @@
-import React from "react";
+import React from 'react';
 
-import "./styles.css";
+import './styles.css';
 
-import { Grid, Row, Col } from "@nordnet/grid";
-
-export const Page = ({ children }) => (
-  <Grid>
-    <Row>
-      <Col xs={12}>{children}</Col>
-    </Row>
-  </Grid>
-);
+export const Page = ({ children }) => <div className="page">{children}</div>;
