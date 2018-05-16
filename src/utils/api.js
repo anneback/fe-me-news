c91f6eeeef04fff04b33813788dd8523641fd225
Jesper Annebäck <jesper.anneback@nordnet.se>
2018-05-16 15:40:31 +0200

    removed comments

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,13 +9,11 @@ const getItems = () => {
   return fetchJson(`${BASE}/v0/topstories.json`).then(res => {
     return first10(res);
   });
-  // TODO: fetch the endpoint, get first 10, return it
 };
 
 // Item endpoint https://hacker-news.firebaseio.com/v0/item/8863.json
 const getItem = id => {
   return fetchJson(`${BASE}/v0/item/${id}.json`);
-  // TODO: fetch relevant item, return it
 };
 
 export const api = {
